5fafe2d365e4b17cf58eb50b66cde2fd6cb4c166
admin <admin@chef-test.localdomain>
2013-12-24 10:04:47 +0000

    test now

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,8 +1,8 @@
-cookbook_path    ["/cygdrive/d/vagrants/chef-test/chef-repo/cookbooks", "/cygdrive/d/vagrants/chef-test/chef-repo/site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-#knife[:berkshelf_path] = "cookbooks"
+log_level                :info
+log_location             STDOUT
+node_name                'vagrant'
+client_key               '/home/admin/tmp/chef-test/chef-repo/.chef/vagrant.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://chef-test:443'
+syntax_check_cache_path  '/home/admin/tmp/chef-test/chef-repo/.chef/syntax_check_cache'
--- chef-repo/site-cookbooks/nginx/recipes/default.rb
+++ chef-repo/site-cookbooks/nginx/recipes/default.rb
@@ -15,34 +15,33 @@ end
 
 
 # git clone nginx 
-execute "git-clone-nginx" do
-	command &lt:&lt;-EOC
-	 mkdir -p ~/src
+bash "git-clone-nginx" do
+	 user "root"
+	 code <<-EOC
+	mkdir -p ~/src
 	 git clone https://github.com/nginx/nginx.git ~/src/nginx
 	 cd ~/src/nginx 
-	 # v1.4.4のタグでブランチを作成 バージョンの指定はここでやる
 	 git checkout -b v1.4.4 refs/tags/v1.4.4
 	 ./configure --user=nginx --group=nginx --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module --with-http_image_filter_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module
 	
 	 make
-	 sudo make install
-	 sudo ln -s /usr/local/nginx/sbin/nginx /sbin/nginx
+	 make install
+	 ln -s /usr/local/nginx/sbin/nginx /sbin/nginx
 	 EOC
 end
 
-service "nginx" do
-	 supports :status=>true, :restart=>true, :reload=>true
-	 action[:enable , :start]
-end
+#service "nginx" do
+#	 supports :status=>true, :restart=>true, :reload=>true
+#	 action[:enable , :start]
+#end
 
 
-templates "nginx.conf" do
-	 path "/etc/nginx/nginx.conf"
-	 source "nginx.conf.erb"
-	 owner "root"
-	 group "root"
-	 mode 0644
-	 notifies :reload, 'service[nginx]"
-end
-
+#templates "nginx.conf" do
+#	 path "/etc/nginx/nginx.conf"
+#	 source "nginx.conf.erb"
+#	 owner "root"
+#	 group "root"
+#	 mode 0644
+#	 notifies :reload, 'service[nginx]"
+#end
 
